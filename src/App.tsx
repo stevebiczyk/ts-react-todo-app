6575fddd3056379f87fcf3c9a59eba1e60133f23
stevebiczyk <stevebiczyk@gmail.com>
2024-09-14 13:55:02 +0100

    created custom MaterialUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React, {FC, ReactElement} from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './themes/customTheme';
 
 const App: FC = (): ReactElement => {
-  return <h1>Hello Alex</h1>
+  return (
+  <ThemeProvider theme={customTheme}>
+    <CssBaseline />
+    <h1>Hello Alex</h1>
+  </ThemeProvider>
+  );
 };
 
 export default App;
