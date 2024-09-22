36635ede29604775e68f6801293eaed85fce5a59
stevebiczyk <stevebiczyk@gmail.com>
2024-09-22 10:41:30 +0100

    added props to DateField

--- src/components/createTaskForm/_taskDateField.tsx
+++ src/components/createTaskForm/_taskDateField.tsx
@@ -1,26 +1,41 @@
-import  React, {FC, ReactElement, useState} from 'react';
+import  React, {FC, ReactElement} from 'react';
 
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-// import { IDateField } from './interfaces/IDateField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import PropTypes from 'prop-types';
 import { TextField, TextFieldProps } from '@mui/material';
+import { IDateField } from './interfaces/iDateField';
 
 
-export const TaskDateField: FC = (): ReactElement => {
-    // state
-    const [date, setDate] = useState<Date | null>(null);
+export const TaskDateField: FC<IDateField> = (
+    props,
+): ReactElement => {
+    //  Destructure props
+    const {
+        value = new Date(),
+        disabled = false,
+        onChange = (date) => console.log(date),
+    } = props;
 
     return (
+        <>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
             <DesktopDatePicker
                 label="Task Date"
                 // inputFormat="dd/MM/yyyy"
-                value={date}
-                onChange={(newValue) => setDate(newValue)}
+                value={value}
+                onChange={onChange}
+                disabled={disabled}
                 renderInput={(params: TextFieldProps) => (<TextField {...params} />)}
             />
         </LocalizationProvider>
+        </>
     );
+};
+
+TaskDateField.propTypes = {
+    value: PropTypes.instanceOf(Date),
+    disabled: PropTypes.bool,
+    onChange: PropTypes.func,
 };
---
+++ src/components/createTaskForm/interfaces/iDateField.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { IDisabled } from './IDisabled';
+
+export interface IDateField extends IDisabled {
+  value?: Date | null;
+  onChange?: (date: Date | null) => void;
+}
