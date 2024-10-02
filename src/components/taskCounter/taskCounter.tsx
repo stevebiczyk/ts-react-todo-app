3f96951db06a3b895e91e6397036fabdb21f0801
stevebiczyk <stevebiczyk@gmail.com>
2024-10-02 19:20:00 +0100

    added dinamic label to TaskCounter

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -3,6 +3,8 @@ import { Avatar, Box, Typography } from "@mui/material";
 import { ITaskCounter } from "./interfaces/ITaskCounter";
 import { Status } from "../createTaskForm/enums/Status";
 import { emitCorrectBorderColor } from "./helpers/emitCorrectBorderColor";
+import { emitCorrectLabel } from "./helpers/emitCorrectLabel";
+import PropTypes from "prop-types";
 
 export const TaskCounter: FC<ITaskCounter> = (
     props,
@@ -27,16 +29,21 @@ export const TaskCounter: FC<ITaskCounter> = (
                 borderColor: `${emitCorrectBorderColor(status)}`,
                 }}
             >
-                <Typography color="#ffffff" variant="h4">10</Typography>
+                <Typography color="#ffffff" variant="h4">{count}</Typography>
             </Avatar>
                 <Typography 
                 color="#ffffff"
                 fontWeight="bold"
                 fontSize="20px"
                 variant="h5"
-
-                >Subtitle</Typography>
+                >{emitCorrectLabel(status)}
+                </Typography>
         </Box>
         </>
     )
+};
+
+TaskCounter.propTypes = {
+    count: PropTypes.number,
+    status: PropTypes.oneOf([Status.todo, Status.inProgress, Status.completed]),
 };
