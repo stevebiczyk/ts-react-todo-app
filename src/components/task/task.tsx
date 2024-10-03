27333fa60c5adb6190a37c204672d89f4610a26e
stevebiczyk <stevebiczyk@gmail.com>
2024-10-03 16:32:07 +0100

    added props to Task component

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -3,8 +3,21 @@ import { Box } from '@mui/material';
 import { TaskHeader } from './_taskHeader';
 import { TaskFooter } from './_taskFooter';
 import { TaskDescription } from './_taskDescription';
+import { ITask } from './interfaces/ITask';
+import { Status } from '../createTaskForm/enums/Status';
+import { Priority } from '../createTaskForm/enums/Priority';
 
-export const Task: FC = (props): ReactElement => {
+export const Task: FC<ITask> = (props): ReactElement => {
+    // destructuring props
+    const { 
+        title = "Task Title",
+        date = new Date(),
+        description = "Task Description", 
+        priority = Priority.medium, 
+        status = Status.completed,
+        onStatusChange = (e) => console.log(e),
+        onClick = (e) => console.log(e),
+    } = props;
     return ( 
     <Box 
     display="flex"
@@ -21,9 +34,9 @@ export const Task: FC = (props): ReactElement => {
         borderColor: "error.light",
     }}
     >
-        <TaskHeader />
-        <TaskDescription />
-        <TaskFooter />
+        <TaskHeader title={title} date={date} />
+        <TaskDescription description={description}/>
+        <TaskFooter onClick={onClick} onStatusChange={onStatusChange} />
     </Box>
     )
 };
