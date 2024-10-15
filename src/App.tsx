2a79b5f3e8840f56a48e96b698a1724afb18e691
stevebiczyk <stevebiczyk@gmail.com>
2024-10-15 23:46:06 +0100

    installed and set up Tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React, { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { customTheme } from './themes/customTheme';
 import { Dashboard } from './pages/dashboard';
 
+// Create a client
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
