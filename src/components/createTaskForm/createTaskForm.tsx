c450c025d6319c6c01ae5f293beecf3954b4d05c
stevebiczyk <stevebiczyk@gmail.com>
2024-10-16 19:29:08 +0100

    added ICreateTask Interface and used Mutation to create new task

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -15,6 +15,9 @@ import { TaskDateField } from './_taskDateField';
 import { TaskSelectField } from './_taskSelectField';
 import { Status } from './enums/Status';
 import { Priority } from './enums/Priority';
+import { useMutation } from '@tanstack/react-query';
+import { sendApiRequest } from '../../helpers/sendApiRequest';
+import { ICreateTask } from '../taskArea/interfaces/ICreateTask';
 
 export const CreateTaskForm: FC = (): ReactElement => {
   // Declare component states
@@ -23,6 +26,11 @@ export const CreateTaskForm: FC = (): ReactElement => {
   const [date, setDate] = useState<Date | null>(new Date());
   const [status, setStatus] = useState<string>(Status.todo);
   const [priority, setPriority] = useState<string>(Priority.medium);
+
+  // Create task mutation
+  const createTaskMutation = useMutation((data: ICreateTask) => {
+    sendApiRequest('http://localhost:3200/tasks', 'POST', data);
+  });
 
   return (
     <Box
---
+++ src/components/taskArea/interfaces/ICreateTask.ts
@@ -0,0 +1,7 @@
+export interface ICreateTask {
+  title: string;
+  description: string;
+  date: Date;
+  status: string;
+  priority: string;
+}
