40598e50bb226aa2997177781026300db18821c4
stevebiczyk <stevebiczyk@gmail.com>
2024-10-26 15:04:42 +0100

    added query data loop to taskArea

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -7,6 +7,7 @@ import { Task } from '../task/task';
 import { useQuery } from '@tanstack/react-query';
 import { sendApiRequest } from '../../helpers/sendApiRequest';
 import { ITaskAPI } from './interfaces/ITaskAPI';
+import { id, is } from 'date-fns/locale';
 
 // export const TaskArea: FC = (): ReactElement => {
 //   const { error, isLoading, data, refetch } = useQuery('tasks', async () => {
@@ -49,20 +50,37 @@ export const TaskArea: FC = (): ReactElement => {
           <TaskCounter />
         </Grid>
         <Grid item display="flex" flexDirection="column" xs={10} md={8}>
-          {error && (
-            <Alert severity="error">
-              There was an error fetching the tasks
-            </Alert>
-          )}
-          {!error && Array.isArray(data) && data.length === 0 && (
-            <Alert severity="info">
-              You do not have any tasks yet. Start by creating some tasks.
-            </Alert>
-          )}
-
-          <Task id="id1" />
-          <Task id="id1" />
-          <Task id="id1" />
+          <>
+            {error && (
+              <Alert severity="error">
+                There was an error fetching the tasks
+              </Alert>
+            )}
+            {!error && Array.isArray(data) && data.length === 0 && (
+              <Alert severity="info">
+                You do not have any tasks yet. Start by creating some tasks.
+              </Alert>
+            )}
+            {isLoading ? (
+              <LinearProgress />
+            ) : (
+              Array.isArray(data) &&
+              data.length > 0 &&
+              data.map((each, index) => {
+                return (
+                  <Task
+                    key={index + each.priority}
+                    id={each.id}
+                    title={each.title}
+                    date={new Date(each.date)}
+                    description={each.description}
+                    priority={each.priority}
+                    status={each.status}
+                  />
+                );
+              })
+            )}
+          </>
         </Grid>
       </Grid>
     </Grid>
