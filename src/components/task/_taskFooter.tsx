7a49098c30d6763ec04ed43c40b0ac323fcce95e
stevebiczyk <stevebiczyk@gmail.com>
2024-10-30 06:26:33 +0000

    added onStatusChangeHandler to inProgress slider in taskArea

--- src/components/task/_taskFooter.tsx
+++ src/components/task/_taskFooter.tsx
@@ -1,82 +1,18 @@
-// import React, { FC, ReactElement } from 'react';
-
-// import { Box, Button, Switch, FormControlLabel } from '@mui/material';
-// import { ITaskFooter } from './interfaces/ITaskFooter';
-// import PropTypes from 'prop-types';
-// import { Status } from '../createTaskForm/enums/Status';
-
-// export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
-//   //destructuring the props
-//   const {
-//     id,
-//     status,
-//     onStatusChange = (e) => console.log(e),
-//     onClick = (e) => console.log(e),
-//   } = props;
-//   return (
-//     <Box
-//       display="flex"
-//       justifyContent="space-between"
-//       alignItems="center"
-//       mt={4}
-//     >
-//       <FormControlLabel
-//         label="In Progress"
-//         control={
-//           <Switch
-//             onChange={onStatusChange}
-//             color="warning"
-//             defaultChecked={status === Status.inProgress}
-//           />
-//         }
-//       />
-//       <Button
-//         variant="contained"
-//         color="success"
-//         size="small"
-//         sx={{ color: 'white' }}
-//         onClick={onClick}
-//       >
-//         Mark as Completed
-//       </Button>
-//     </Box>
-//   );
-// };
-
-// TaskFooter.propTypes = {
-//   onStatusChange: PropTypes.func,
-//   onClick: PropTypes.func,
-//   id: PropTypes.string.isRequired,
-//   status: PropTypes.string,
-// };
 import React, { FC, ReactElement } from 'react';
+
 import { Box, Button, Switch, FormControlLabel } from '@mui/material';
+import { ITaskFooter } from './interfaces/ITaskFooter';
+import PropTypes from 'prop-types';
 import { Status } from '../createTaskForm/enums/Status';
 
-interface ITaskFooter {
-  id: string;
-  status?: string;
-  onStatusChange?: (
-    event: React.ChangeEvent<HTMLInputElement>,
-    id: string,
-  ) => void;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>, id: string) => void;
-}
-
-export const TaskFooter: FC<ITaskFooter> = ({
-  id,
-  status = Status.todo,
-  onStatusChange = () => {},
-  onClick = () => {},
-}): ReactElement => {
-  const handleStatusChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onStatusChange(event, id);
-  };
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onClick(event, id);
-  };
-
+export const TaskFooter: FC<ITaskFooter> = (props): ReactElement => {
+  //destructuring the props
+  const {
+    id,
+    status,
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
   return (
     <Box
       display="flex"
@@ -88,9 +24,9 @@ export const TaskFooter: FC<ITaskFooter> = ({
         label="In Progress"
         control={
           <Switch
-            onChange={handleStatusChange}
+            onChange={(e) => onStatusChange(e, id)}
             color="warning"
-            checked={status === Status.inProgress}
+            defaultChecked={status === Status.inProgress}
           />
         }
       />
@@ -99,10 +35,65 @@ export const TaskFooter: FC<ITaskFooter> = ({
         color="success"
         size="small"
         sx={{ color: 'white' }}
-        onClick={handleClick}
+        onClick={(e) => onClick(e, id)}
       >
         Mark as Completed
       </Button>
     </Box>
   );
 };
+
+TaskFooter.propTypes = {
+  onStatusChange: PropTypes.func,
+  onClick: PropTypes.func,
+  id: PropTypes.string.isRequired,
+  status: PropTypes.string,
+};
+// import React, { FC, ReactElement } from 'react';
+// import { Box, Button, Switch, FormControlLabel } from '@mui/material';
+// import { Status } from '../createTaskForm/enums/Status';
+// import { ITaskFooter } from './interfaces/ITaskFooter';
+
+// export const TaskFooter: FC<ITaskFooter> = ({
+//   id,
+//   status = Status.todo,
+//   onStatusChange = () => {},
+//   onClick = () => {},
+// }): ReactElement => {
+//   const handleStatusChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     onStatusChange(event, id);
+//   };
+
+//   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//     onClick(event, id);
+//   };
+
+//   return (
+//     <Box
+//       display="flex"
+//       justifyContent="space-between"
+//       alignItems="center"
+//       mt={4}
+//     >
+//       <FormControlLabel
+//         label="In Progress"
+//         control={
+//           <Switch
+//             onChange={handleStatusChange}
+//             color="warning"
+//             checked={status === Status.inProgress}
+//           />
+//         }
+//       />
+//       <Button
+//         variant="contained"
+//         color="success"
+//         size="small"
+//         sx={{ color: 'white' }}
+//         onClick={handleClick}
+//       >
+//         Mark as Completed
+//       </Button>
+//     </Box>
+//   );
+// };
--- src/components/task/interfaces/ITaskFooter.ts
+++ src/components/task/interfaces/ITaskFooter.ts
@@ -3,10 +3,20 @@ import React from 'react';
 export interface ITaskFooter {
   id: string;
   status?: string;
-  onStatusChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onStatusChange?: (e: React.ChangeEvent<HTMLInputElement>, id: string) => void;
   onClick?: (
     e:
       | React.MouseEvent<HTMLButtonElement>
       | React.MouseEvent<HTMLAnchorElement>,
+    id: string,
   ) => void;
 }
+// interface ITaskFooter {
+//   id: string;
+//   status?: string;
+//   onStatusChange?: (
+//     event: React.ChangeEvent<HTMLInputElement>,
+//     id: string,
+//   ) => void;
+//   onClick?: (event: React.MouseEvent<HTMLButtonElement>, id: string) => void;
+// }
