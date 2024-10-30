c4d1c95e11a32528837fbf9f2dcc7137f4c74914
stevebiczyk <stevebiczyk@gmail.com>
2024-10-30 14:25:01 +0000

    created React Context and added it to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { customTheme } from './themes/customTheme';
 import { Dashboard } from './pages/dashboard';
+import ComposeContext from './context/compose.context';
+import { rootContext } from './context/root.context';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -12,11 +14,13 @@ const queryClient = new QueryClient();
 const App: FC = (): ReactElement => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={customTheme}>
-        <CssBaseline />
-        <Dashboard />
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ComposeContext components={rootContext}>
+        <ThemeProvider theme={customTheme}>
+          <CssBaseline />
+          <Dashboard />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ComposeContext>
     </QueryClientProvider>
   );
 };
