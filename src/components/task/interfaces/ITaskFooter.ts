c51c7c57cff686142336345ae505cb6171a65138
stevebiczyk <stevebiczyk@gmail.com>
2024-10-30 06:55:25 +0000

    added markCompleteHandler onClickHandler function

--- src/components/task/interfaces/ITaskFooter.ts
+++ src/components/task/interfaces/ITaskFooter.ts
@@ -11,12 +11,3 @@ export interface ITaskFooter {
     id: string,
   ) => void;
 }
-// interface ITaskFooter {
-//   id: string;
-//   status?: string;
-//   onStatusChange?: (
-//     event: React.ChangeEvent<HTMLInputElement>,
-//     id: string,
-//   ) => void;
-//   onClick?: (event: React.MouseEvent<HTMLButtonElement>, id: string) => void;
-// }
